d8b7aedea87462febfdac8d440903d17f30278e5
Kira1108 <327485253@163.com>
2024-02-07 13:42:44 +0800

    Initial commit

---
+++ run_jupyter_mac.sh
@@ -0,0 +1,5 @@
+docker run -dp 9876:8888 \
+--name ju-torch \
+-v "$(pwd)":/home/jovyan/work \
+quay.io/jupyter/pytorch-notebook start-notebook.py \
+--PasswordIdentityProvider.hashed_password='argon2:$argon2id$v=19$m=10240,t=10,p=8$Aay1/OtkKPYM+iiZ36tBvQ$ANsLOEPKzbhLBa9mz83W7h2Kh/bgBzEcgeNJPuDul8E'
